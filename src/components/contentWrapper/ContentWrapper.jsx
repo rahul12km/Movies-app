e1540b427f802404a968fa708969575ddd82fd4a
rahul12km <bestrahul123r@gmail.com>
2024-02-22 11:57:36 +0530

    Initial commit

---
+++ src/components/contentWrapper/ContentWrapper.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+import "./style.scss";
+
+const ContentWrapper = ({ children }) => {
+  return <div className="contentWrapper">{children}</div>;
+};
+
+export default ContentWrapper;
---
+++ src/pages/404/PageNotFound.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./style.scss";
+const PageNotFound = () => {
+  return <div>PageNotFound</div>;
+};
+
+export default PageNotFound;
